deace6ff1dc94f657bacd6e82868cba915c66987
Vot <vot@siliconfen.co>
2020-08-31 08:55:55 +0100

    Update journals mock data

--- data/journals.js
+++ data/journals.js
@@ -2,5 +2,16 @@ module.exports = [
 	{ id: 'ACE', name: 'American Journal of Countless Examples', publisher: 'AUP' },
 	{ id: 'BPP', name: 'British Journal of Practical Puns', publisher: 'BUP' },
 	{ id: 'JBR', name: 'Journal of Believable Research', publisher: 'JPub' },
-	{ id: 'eLife', name: 'eLife', publisher: 'eLife' },
+	{
+		id: 'eLife',
+		name: 'eLife',
+		publisher: {
+			name: 'eLife',
+			url: 'https://elifesciences.org/'
+		},
+		profile: `
+			eLife works to improve research communication through open science and open technology innovation.
+			eLife is a non-profit organisation inspired by research funders and led by scientists.
+			Our mission is to help scientists accelerate discovery by operating a platform for research communication that encourages and recognises the most responsible behaviours in science.`,
+	},
 ];
