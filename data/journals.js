2c81e43939979b6db99b7db53f000c98fc5bbb2f
Vot <vot@siliconfen.co>
2020-09-03 03:00:34 +0100

    Update journal data

--- data/journals.js
+++ data/journals.js
@@ -1,34 +1,37 @@
 module.exports = [
 	{
-		id: 'JBR',
-		name: 'Journal of Believable Research',
-		publisher: {
-			name: 'Open Publishing Platform',
-			url: 'https://OpenPublishingPlatform.com/'
-		},
-		description: `Journal of Believable Research is a fictional journal, invented to demonstrate the Open Publishing Platform.`,
-		editorialBoard: [],
-		identifiers: {
-			mnemonic: 'JBR',
-			doi: '10.0000/0000',
-			issnPrint: '0000-0000',
-			issnOnline: '0000-0000',
-		}
-	},
-	{
 		id: 'eLife',
 		name: 'eLife Sciences',
 		publisher: {
-			name: 'eLife',
+			name: 'eLife Sciences Publications Ltd',
 			url: 'https://elifesciences.org/'
 		},
 		description: `eLife is a selective, not for profit peer-reviewed open access scientific journal for the biomedical and life sciences. It was established at the end of 2012 by the Howard Hughes Medical Institute, Max Planck Society, and Wellcome Trust, following a workshop held in 2010 at the Janelia Farm Research Campus.`,
-		editorialBoard: [],
-		identifiers: {
+		editorialBoard: [{
+			role: 'chief-editor',
+			name: 'Michael Eisen'
+		}],
+		indexing: {
 			mnemonic: 'eLife',
-			doi: '10.0000/0000',
-			issnPrint: '0000-0000',
-			issnOnline: '0000-0000',
-		}
-	},
+			iso4: 'eLife', // ISO4 abbreviation
+			// doi: '10.0000/0000',
+			// issnPrint: '0000-0000',
+			issnOnline: '2050-084X',
+			coden: 'ELIFA8',
+			oclc: '813236730',
+		},
+		links: {
+			researchgate: 'https://www.researchgate.net/journal/2050-084X_eLife_Sciences'
+		},
+		frequency: 'continuous',
+		openAccess: 'full',
+		impactFactor: {
+			score: '7.080',
+			year: 2019
+		},
+		history: {
+			startDate: 2012
+		},
+		disciplines: ['biomedicine', 'life sciences']
+	}
 ];
