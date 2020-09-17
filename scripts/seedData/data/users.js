ce3166ea3fc2896d25a3033909bf00e4f042217b
Vot <vot@siliconfen.co>
2020-09-18 00:14:59 +0100

    Add seedData script

---
+++ scripts/seedData/data/users.js
@@ -0,0 +1,11 @@
+module.exports = [
+  {
+    email: 'admin@localhost',
+    firstname: '',
+    lastname: 'Admin',
+    password: '',
+    dateCreated: '2020-01-01 00:00:00.000',
+    dateLastActivity: '2020-01-01 00:00:00.000',
+    userGroup: 1,
+  }
+];
---
+++ scripts/seedData/index.js
@@ -0,0 +1,38 @@
+const _ = require('lodash');
+const MongoLib = require('../../lib/mongo');
+
+const data = {
+  pages: require('./data/pages'),
+  journals: require('./data/journals'),
+  users: require('./data/users'),
+  // subjects: require('./data/subjects')
+};
+
+function insertData(data, collection, key, callback) {
+  MongoLib[collection].find({}, (err, existingResults) => {
+    const missingItems = _.differenceBy(data, existingResults, key);
+    if (!missingItems.length) {
+      console.log(`All required items exist in collection "${collection}"`);
+      return callback();
+    }
+
+    MongoLib[collection].insertMany(missingItems, (err, data) => {
+      if (err) {
+        console.log('Error:', err);
+        return callback(err);
+      }
+
+      console.log(`Inserted ${missingItems.length} missing items into collection "${collection}": ${_.map(missingItems, 'key').join(', ')}`);
+      return callback();
+    });
+  });
+}
+
+insertData(data.pages, 'pages', 'url', () => {
+  insertData(data.journals, 'journals', 'id', () => {
+    insertData(data.users, 'users', 'email', () => {
+      console.log('Finished inserting seed data.')
+      process.exit(0);
+    });
+  });
+});
